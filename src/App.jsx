1953c4a6e6f07bdaf5b289c25d50f986f860745a
Your Name <you@example.com>
2023-06-30 01:07:49 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <News />
+      <SearchForm />
+      <Banner img={} />
+      <Widgets />
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/News/New.jsx
@@ -0,0 +1,7 @@
+
+// Новость
+function New(props) {
+    return <div className="new">{props.children}</div>
+}
+
+export default New;
